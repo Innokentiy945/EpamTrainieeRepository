6d74aa9316483ac6dc9cabc8a422159389ee7335
Innokentiy945 <51906417+Innokentiy945@users.noreply.github.com>
2020-04-02 14:06:15 +0300

    Update

---
+++ src/test/java/hw1/Trigonometry/CosineTest.java
@@ -0,0 +1,28 @@
+package hw1.Trigonometry;
+
+import hw1.CalculatorSetUp;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CosineTest extends CalculatorSetUp {
+
+
+
+    @Test(dataProvider = "dataProviderDoubleNumbers", groups = "trigonometry")
+    public void cosNumbers(double a, double expected) {
+        double actual = calculator.cos(a);
+        assertEquals(actual, expected);
+    }
+
+
+    @DataProvider
+    public Object[][] dataProviderDoubleNumbers() {
+        return new Object[][] {
+                {55.0, -0.9997551733586199},
+                {-105, 0.9705352835374847},
+                {8.3, 0.9021718337562933}
+        };
+    }
+}
