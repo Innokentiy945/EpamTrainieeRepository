f4b122b8f8c47fb30ab064c0c054964d2f41125f
Kesha84 <forjimmy945@gmail.com>
2020-04-11 01:16:47 +0300

    Add allure listeners

--- src/test/java/hw5/Tests/Ex1/FunctionalTest1.java
+++ src/test/java/hw5/Tests/Ex1/FunctionalTest1.java
@@ -4,16 +4,13 @@ package hw5.Tests.Ex1;
 import hw5.Pages.HomePage;
 import hw5.StepsPattern.HomePageSteps;
 import hw5.Tests.TestBase.TestBase;
+import hw5.utils.AllureScreenshotListener;
 import io.qameta.allure.Story;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.testng.Assert.assertEquals;
-
+@Listeners(AllureScreenshotListener.class)
 public class FunctionalTest1 extends TestBase {
 
     protected HomePageSteps homePageSteps;
