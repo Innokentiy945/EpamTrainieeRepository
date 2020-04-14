e570a5e2384af505ce07ff0051b4b6b0506588e4
Innokentiy945 <51906417+Innokentiy945@users.noreply.github.com>
2020-04-14 13:15:36 +0300

    Add files via upload

---
+++ src/main/java/hw5/Composite/Benefits.java
@@ -0,0 +1,44 @@
+package hw5.Composite;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Benefits extends AbstractPageComposite {
+
+    public Benefits(WebDriver driver) throws InterruptedException {
+        super(driver);
+    }
+
+    @FindBy(className = "benefit-icon")
+    private List<WebElement> benefitIcons;
+
+    @FindBy(className = "benefit-txt")
+    private List<WebElement> benefitIconsTexts;
+
+    public int benefitImagesCount(){
+        return wait.until(ExpectedConditions.visibilityOfAllElements(benefitIcons)).size();
+    }
+
+    public int benefitTextsCount(){
+        return wait.until(ExpectedConditions.visibilityOfAllElements(benefitIconsTexts)).size();
+    }
+
+    public List<String> benefitTexts(){
+        wait.until(ExpectedConditions.visibilityOfAllElements(benefitIconsTexts));
+        return benefitIconsTexts.stream().map(WebElement::getText).collect(Collectors.toList());
+    }
+
+    public boolean isBenefitIconsDisplayed(){
+        wait.until(ExpectedConditions.visibilityOfAllElements(benefitIconsTexts));
+        boolean isDisplayed = false;
+        for (WebElement element : benefitIconsTexts) {
+            isDisplayed = element.isDisplayed();
+        }
+        return isDisplayed;
+    }
+}
