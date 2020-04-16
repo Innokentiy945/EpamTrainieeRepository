a3695cca84b974079ea196559dd8c65af65997f5
Kesha84 <forjimmy945@gmail.com>
2020-04-16 16:57:00 +0300

    Merge branch 'homework5' of https://github.com/Innokentiy945/InnokentiiObraztcov into homework5
    # Conflicts:
    #	pom.xml
    #	src/main/java/hw5/Composite/AbstractPageComposite.java
    #	src/main/java/hw5/Composite/Benefits.java
    #	src/main/java/hw5/Composite/Frame.java
    #	src/main/java/hw5/Composite/HeaderMenu.java
    #	src/main/java/hw5/Composite/LeftMenu.java
    #	src/main/java/hw5/Composite/Services.java
    #	src/main/java/hw5/Pages/AbstractPage.java
    #	src/main/java/hw5/Pages/DifferentElementPage.java
    #	src/main/java/hw5/Pages/HomePage.java
    #	src/main/java/hw5/utils/PropertiesOpener.java
    #	src/test/java/hw5/StepsPattern/DifferentElementPageSteps.java
    #	src/test/java/hw5/Tests/Ex1/FunctionalTest1.java
    #	src/test/java/hw5/Tests/Ex1/FunctionalTest2.java
    #	src/test/java/hw5/Tests/Ex2/FunctionalTest1Failed.java
    #	src/test/java/hw5/Tests/TestBase/TestBase.java
    #	src/test/java/hw5/utils/AllureScreenshotListener.java
    #	src/test/java/hw5/utils/WebDriverSingleton.java

---
+++ src/test/java/hw5/Tests/Ex1/FunctionalTest1.java
@@ -0,0 +1,42 @@
+package hw5.Tests.Ex1;
+
+import hw5.StepsPattern.HomePageSteps;
+import hw5.Tests.TestBase.TestBase;
+import hw5.utils.AllureScreenshotListener;
+import io.qameta.allure.Story;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+@Listeners(AllureScreenshotListener.class)
+public class FunctionalTest1 extends TestBase {
+
+    protected HomePageSteps homePageSteps;
+
+    @Story("Log in to site and checking web elements")
+
+    @BeforeMethod
+    public void init() throws InterruptedException {
+        homePageSteps = new HomePageSteps(driver);
+    }
+
+    @Test
+    public void functionalTest() throws InterruptedException {
+
+        standartOperations();
+
+        homePageSteps.verifyingHeaderMenuText(Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"));
+        homePageSteps.verifyingNumberOfItems(Arrays.asList("HOME", "CONTACT FORM", "SERVICE", "METALS & COLORS"));
+        homePageSteps.headerMenuIsDisplaeyd();
+        homePageSteps.verifyingIframeExist();
+        homePageSteps.verifyingIframeButton();
+        homePageSteps.verifyingSwitchiToPrevousPage();
+        homePageSteps.verifyingItemsInLeftMenu();
+        homePageSteps.veryfyingNamesOfItems(Arrays.asList("Home", "Contact form", "Service", "Metals & Colors", "Elements packs"));
+        homePageSteps.verifyingNumbersOfItems(Arrays.asList("Home", "Contact form", "Service", "Metals & Colors", "Elements packs"));
+
+    }
+}
+
